14d3b3a4986c31bd385bef67c4147d332f859993
frest1ler <143139569+frest1ler@users.noreply.github.com>
2025-03-14 16:45:30 +0300

    init of bypass
    refactoring: added first version of bypass

--- dump.cpp
+++ dump.cpp
@@ -4,7 +4,13 @@
 #include "dump.h"
 
 void dump_first_node(Tree* tree, FILE * point_to_file);
-void dump_node(int number_node, Node* node, FILE * point_to_file);
+void dump_node(Node* node, FILE * point_to_file);
+
+
+int  use_left(Node* node);
+int  go_left (Node* node);
+void go_back (Node* node);
+void bypass  (Tree* tree);
 
 void dump(Tree* tree, char* fname)
 {
@@ -23,26 +29,29 @@ void dump(Tree* tree, char* fname)
     
     dump_first_node(tree, point_to_file);
 
-    int i = 1;
-    Node* node = (tree->root)->left;
+    //Node* node = tree->root;
 
-    dump_node(i, node, point_to_file);    
+    bypass(tree);
+
+    //dump_node(node, point_to_file);    
 
     fprintf(point_to_file, "\n}\n}");
 
     fclose(point_to_file);
 }
 
-void dump_node(int i, Node* node, FILE * point_to_file)
-{
-    fprintf(point_to_file, "subgraph cluster_A_left {\nlabel=\"Левое облачко A1\";\n style=dotted;\nnode [shape=record];\n");
+void dump_node(Node* node, FILE * point_to_file)
+{   
+    fprintf(point_to_file, "subgraph cluster_A_left {\nlabel=\"Левое облачко A1\";\n");
+    fprintf(point_to_file, "style=dotted;\nnode [shape=record];\n");
    
-    fprintf(point_to_file, "a%d [label=\"{Parent %p | Ptr %p | Data %d | {Left %p | Right %p }}\"];\n", i, node->parent, node, node->data, node->left, node->right);
-    if (i == 1){
-        fprintf(point_to_file, "a -> a1 [label=\"Left\" dir=forward];\n}\n");
+    fprintf(point_to_file, "a%p [label=\"{Parent %p | Ptr %p | Data %d | {Left %p | Right %p }}\"];\n", node, node->parent, node, node->data, node->left, node->right);
+    
+    if ((node->parent)->left == node){
+    fprintf(point_to_file, "a%p -> a%p [label=\"Left\" dir=forward];\n}\n", node->parent, node);
     }
     else{
-        fprintf(point_to_file, "a%d -> a%d [label=\"Left\" dir=forward];\n}\n", i - 1, i);
+    fprintf(point_to_file, "a%p -> a%p [label=\"Right\" dir=forward];\n}\n", node->parent, node);
     }
 }
 
@@ -56,5 +65,54 @@ void dump_first_node(Tree* tree, FILE * point_to_file)
 
     fprintf(point_to_file, "subgraph cluster_A {label=\"Облачко A\";style=dashed;node [shape=record];\n");
 
-    fprintf(point_to_file, "a [label=\"{Parent %p | Ptr %p | Type %d | Data %d | {Left %p | Right %p }}\"];\n root -> a;", tree, tree->root, type, (tree->root)->data, (tree->root)->left, (tree->root)->right);
+    fprintf(point_to_file, "a%p [label=\"{Parent %p | Ptr %p | Type %d | Data %d | {Left %p | Right %p }}\"];\n root -> a;", tree->root,tree, tree->root, type, (tree->root)->data, (tree->root)->left, (tree->root)->right);
+}
+
+int use_left(Node* node)
+{
+    int count_node = 0;
+
+    while(node->left != NULL)
+    {
+        node = node->left;
+
+        count_node++;
+    }
+    return count_node;
+}
+
+void bypass(Tree* tree)
+{
+    int found_size = 0;
+
+    Node* node = tree->root;
+
+    while(found_size < tree->size)
+    {
+        found_size += go_left(node);
+
+        go_back(node);
+    }
+}
+
+int go_left(Node* node)
+{   
+    int add_found_size = 0;
+
+    while(node->right != NULL || node->left != NULL)
+    {
+        add_found_size += use_left(node);
+
+        node = node->right;
+
+        add_found_size += 1;
+    }
+    return add_found_size;
+}
+
+void go_back(Node* node)
+{
+    while(node->right == NULL){
+        node = node->parent;
+    }
 }
